e95110cbcc59a96b2be9da42d649e5595ce533b8
JayvonK <jaykhuth@gmail.com>
2024-03-23 23:41:43 -0700

    adding line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,15 @@ export default function Home() {
       <div className="boxBg h-full w-full py-6 px-10 rounded-lg">
 
         {/* First row of the page */}
-        <div className="grid grid-cols-[45%_50%] ">
+        <div className="grid grid-cols-[52%_6%_42%] ">
 
           <div className="flex justify-between">
             <input className="min-h-12 w-full" type="text" placeholder="Search for a city"/>
             <img src="" alt="" />
+          </div>
+
+          <div>
+
           </div>
 
 
@@ -22,12 +26,16 @@ export default function Home() {
 
 
         {/* Second Row of the page */}
-        <div className="grid grid-cols-[45%_50%]">
+        <div className="grid grid-cols-[52%_6%_42%]">
           <div>
 
           </div>
 
-          <div className="p-5 boxTwoBg rounded-lg">
+          <div className="flex justify-center">
+            <div className="line"></div>
+          </div>
+
+          <div className="p-5 boxTwoBg w-full rounded-lg">
 
             <div className="">
               <h1>5 Day Forecast</h1>
